2d90b328f7d847f4b0566621aa1dc9c802c173dd
felix <xuanphuc.a1gv@gmail.com>
2026-03-29 02:09:29 -0400

    initial specs.md and flake
    Signed-off-by: felix <xuanphuc.a1gv@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Development shell for refurbished marketplace";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          f (
+            import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            }
+          )
+        );
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            gopls
+            gofumpt
+            golangci-lint
+
+            nodejs
+
+            goose
+            sqlc
+
+            protobuf
+            protoc-gen-go
+            protoc-gen-go-grpc
+          ];
+
+          shellHook = ''
+            export GOPATH="$HOME/go"
+            export PATH="$GOPATH/bin:$PATH"
+          '';
+        };
+      });
+    };
+}
